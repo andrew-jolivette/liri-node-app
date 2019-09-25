3eb9a7f036238d136d008595c0d9545c6e7651ff
Andrew J <53879641+andrew-jolivette@users.noreply.github.com>
2019-09-24 19:57:59 -0700

    added menu options and test state responses

--- liri.js
+++ liri.js
@@ -4,8 +4,36 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+var fs = require("fs");
 var axios = require("axios");
 var inquirer = require("inquirer");
 var bandsintown = require("bandsintown");
 var moment = require('moment');
-moment().format();
+// moment().format();
+
+inquirer
+  .prompt([
+    {
+      message: "\tThank you for Using LIRI! What can I help you with?",
+      type: "list",
+      choices: ["Find Concerts", "Spotify This Song", "Movie Information", "do-what-it-says"],
+      name: "choice"
+    }
+  ])
+    .then( res => {
+      console.log(res)
+      switch(res.choice) {
+        case "Find Concerts":
+          console.log('Concerts!')
+          break;
+        case "Spotify This Song":
+          console.log("Songs!")
+          break;
+        case "Movie Information":
+          console.log("movies!")
+          break;
+        case "do-what-it-says":
+          console.log('what say do????!?!?')
+          break;
+      }
+    });
