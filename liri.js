afd34515480dc1a12af61a58a484fa10f19d35bc
Andrew J <53879641+andrew-jolivette@users.noreply.github.com>
2019-09-24 19:25:18 -0700

    added more required dependancies

--- liri.js
+++ liri.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
 var axios = require("axios");
 var inquirer = require("inquirer");
-var bandsintown = require('bandsintown');
-
+var bandsintown = require("bandsintown");
+var moment = require('moment');
+moment().format();
