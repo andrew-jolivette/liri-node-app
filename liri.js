697e870f7d3ccfa2df14c4227bbc448bfa1a7cdd
Andrew J <53879641+andrew-jolivette@users.noreply.github.com>
2019-09-24 19:20:53 -0700

    added required dependancies to liriJS

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var inquirer = require("inquirer");
+var bandsintown = require('bandsintown');
+
