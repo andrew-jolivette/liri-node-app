1c50c643b6beb7ef4705a056c614d1c15be95ec5
Andrew J <53879641+andrew-jolivette@users.noreply.github.com>
2019-09-25 19:49:50 -0700

    main menu selection, bandsintown selection and logic to search again or go back to the main menu

--- liri.js
+++ liri.js
@@ -10,30 +10,105 @@ var inquirer = require("inquirer");
 var bandsintown = require("bandsintown");
 var moment = require('moment');
 // moment().format();
+var menu = function() {
+  inquirer
+    .prompt([
+      {
+        message: "\tThank you for Using LIRI! What can I help you with?",
+        type: "list",
+        choices: ["Find Concerts", "Spotify This Song", "Movie Information", "do-what-it-says"],
+        name: "choice"
+      }
+    ])
+      .then( res => {
+        console.log(res)
+        switch(res.choice) {
+          case "Find Concerts":
+            concerts();
+            break;
+          case "Spotify This Song":
+            console.log("Songs!");
+            break;
+          case "Movie Information":
+            console.log("movies!");
+            break;
+          case "do-what-it-says":
+            console.log('what say do????!?!?');
+            break;
+        }
+      });
+};
 
-inquirer
+menu();
+
+var concerts = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What band or artist are you looking for?",
+        name: "artist"
+      }
+    ])
+      .then ( res => {
+        const artist = res.artist
+        axios
+          .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+            .then( res => {
+              const info = res.data;
+              if (info.length < 1){
+                console.log('\n------------N-O--C-O-N-C-E-R-T-S--F-O-U-N-D------------\n')
+                concertsAgain();
+              }
+              console.log(`Events for "${artist}"`)
+              console.log('\n-------------------------------------------------------\n')
+              for (let i = 0; i < info.length; i++) {
+                console.log(`Venue: ${info[i].venue.name}`);
+                console.log(`Location: ${info[i].venue.city}, ${info[i].venue.region} ${info[i].venue.country}`);
+                console.log(`Time: ${moment(info[i].date).format("dddd, MMMM Do YYYY, h:mm a")}`)
+                console.log('\n-------------------------------------------------------\n')
+              }
+              concertsAgain();
+            })
+            .catch( err => {
+              console.log(err);
+            })
+      })
+};
+
+var backToMenu = function() {
+  inquirer
   .prompt([
     {
-      message: "\tThank you for Using LIRI! What can I help you with?",
-      type: "list",
-      choices: ["Find Concerts", "Spotify This Song", "Movie Information", "do-what-it-says"],
-      name: "choice"
+      type: "confirm",
+      message: "Back to Main Menu?",
+      name: "confirm"
     }
   ])
     .then( res => {
-      console.log(res)
-      switch(res.choice) {
-        case "Find Concerts":
-          console.log('Concerts!')
-          break;
-        case "Spotify This Song":
-          console.log("Songs!")
-          break;
-        case "Movie Information":
-          console.log("movies!")
-          break;
-        case "do-what-it-says":
-          console.log('what say do????!?!?')
-          break;
+      if (res.confirm){
+        menu();
+      } else{
+        return;
       }
-    });
+    })
+};
+
+var concertsAgain = function() {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        message: "Would you like to search for another artist?",
+        name: "confirm"
+        
+      }
+    ])
+      .then( res => {
+        if (res.confirm){
+          concerts();
+        } else {
+          backToMenu();
+        }
+      })
+};
